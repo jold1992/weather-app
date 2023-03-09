a77f2a0d5ce5dff85f11dd915fd6d9808126f9a3
john <jold1992@gmail.com>
2023-03-08 22:30:46 -0600

    feat: interfaz para hacer la petición a la api

---
+++ src/components/Box.jsx
@@ -0,0 +1,127 @@
+import { useState } from 'react'
+import { useRef } from 'react'
+
+function Box() {
+    const container = useRef(null) //document.getElementById('container');
+    const search = useRef(null)  //document.getElementById('btnSearch');
+    const weatherBox = useRef(null) //document.getElementById('weather-box');
+    const weatherDetails = useRef(null) // document.getElementById('weather-details');
+    const error404 = useRef(null) // document.getElementById('not-found');
+    const image = useRef(null) //document.querySelector('.weather-box img');
+    const temperature = useRef(null) //document.querySelector('.weather-box .temperature');
+    const description = useRef(null) //document.querySelector('.weather-box .description');
+    const humidity = useRef(null) //document.querySelector('.weather-details .humidity span');
+    const wind = useRef(null) //document.querySelector('.weather-details .wind span');
+
+    const [city, setCity] = useState('')
+
+    const onChangeText = (e) => {
+        setCity(e.target.value);
+    }
+
+    const onClick = () => {
+
+        const APIKey = 'b7f9cf956efd878631100e01a95e5f73';
+
+        if (city === '')
+            return;
+
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+            .then(response => response.json())
+            .then(json => {
+
+                if (json.cod === '404') {
+                    container.current.style.height = '400px';
+                    weatherBox.current.style.display = 'none';
+                    weatherDetails.current.style.display = 'none';
+                    error404.current.style.display = 'block';
+                    error404.current.classList.add('fadeIn');
+                    return;
+                }
+
+                error404.current.style.display = 'none';
+                error404.current.classList.remove('fadeIn');
+
+                switch (json.weather[0].main) {
+                    case 'Clear':
+                        image.current.src = '/assets/images/clear.png';
+                        break;
+
+                    case 'Rain':
+                        image.current.src = '/assets/images/rain.png';
+                        break;
+
+                    case 'Snow':
+                        image.current.src = '/assets/images/snow.png';
+                        break;
+
+                    case 'Clouds':
+                        image.current.src = '/assets/images/cloud.png';
+                        break;
+
+                    case 'Haze':
+                        image.current.src = '/assets/images/mist.png';
+                        break;
+
+                    default:
+                        image.current.src = '';
+                }
+
+                temperature.current.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+                description.current.innerHTML = `${json.weather[0].description}`;
+                humidity.current.innerHTML = `${json.main.humidity}%`;
+                wind.current.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+
+                weatherBox.current.style.display = '';
+                weatherDetails.current.style.display = '';
+                weatherBox.current.classList.add('fadeIn');
+                weatherDetails.current.classList.add('fadeIn');
+                container.current.style.height = '590px';
+
+
+            });
+
+    }
+
+    return (
+        <main>
+            <h1 className="text-white text-center pb-8">Weather <span className="text-gradient">App</span></h1>
+            <div ref={container} className="container">
+                <div className="search-box">
+                    <i className="fa-solid fa-location-dot"></i>
+                    <input ref={search} type="text" placeholder="Enter your location" value={city} onChange={onChangeText} />
+                    <button className="fa-solid fa-magnifying-glass" onClick={onClick}></button>
+                </div>
+            </div>
+            <div ref={error404} className="not-found">
+                <img src="/assets/images/404.png" />
+                <p>Oops! Invalid location :/</p>
+            </div>
+
+            <div ref={weatherBox} className="weather-box">
+                <img ref={image} src="" />
+                <p ref={temperature} className="temperature"></p>
+                <p ref={description} className="description"></p>
+            </div>
+
+            <div ref={weatherDetails} className="weather-details">
+                <div className="humidity">
+                    <i className="fa-solid fa-water"></i>
+                    <div className="text">
+                        <span ref={humidity}></span>
+                        <p>Humidity</p>
+                    </div>
+                </div>
+                <div className="wind">
+                    <i className="fa-solid fa-wind"></i>
+                    <div className="text">
+                        <span ref={wind}></span>
+                        <p>Wind Speed</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default Box;
