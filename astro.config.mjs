f00683c2fb7e89e7bf738fb966e8e231711d3890
John Lomas <jold1992@gmail.com>
2023-03-11 11:11:46 -0600

    Update astro.config.mjs
    fix: config de astro para deploy con actions de github

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  site: 'https://github.com/jold1992',
+  base: "/weather-app"
 });
